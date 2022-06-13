24377a00c286753d46fd1b7385f31651915c2cb1
Diana Bautista Bautista <ada_wit60@hackademy.mx>
2022-06-13 11:34:11 -0500

    Adding some text in the image

--- create_card.py
+++ create_card.py
@@ -3,8 +3,12 @@ import cv2
 img = cv2.imread("poster.jpg")
 rocket = img[120:360,400:500]
 # this line put the image in this position
-img[0:240,400:500] = rocket
+img[0:240,500:600] = rocket
 # img[0:240,0:100] = rocket
+
+text_to_show = "Developer in Progress"
+
+cv2.putText(img,text_to_show,(20,220),fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,fontScale=1,color=(0,0,255))
 
 cv2.imshow("output",img)
 cv2.waitKey(0)
